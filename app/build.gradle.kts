ca980c6669c723af78e4c0b68946814be6aa8103
Mawinda <mawindavic@gmail.com>
2022-10-11 06:19:53 +0300

    Chore(Build -> App) adds app dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,12 @@
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
+
 plugins {
-    id("com.android.application")
+    id(Plugins.androidApplication)
+    id(Plugins.kotlinAndroid)
+    id(Plugins.kotlinKapt)
+    id(Plugins.kotlinParcelize)
+    id(Plugins.navigation)
+    id(Plugins.hilt)
     id("org.jetbrains.kotlin.android")
 }
 
@@ -38,11 +45,16 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.7.0")
-    implementation("androidx.appcompat:appcompat:1.5.1")
-    implementation("com.google.android.material:material:1.6.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
+    android()
+    navigation()
+    lifecycle()
+    implementation(AndroidLibraries.splashScreen)
+    implementation(Libraries.coil)
+    hilt()
+    implementation(project(Modules.data))
+    test()
+}
+
+kapt {
+    correctErrorTypes = true
 }
