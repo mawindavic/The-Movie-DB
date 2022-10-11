cd727c7f0e31bea58d0b8f48511d510d64c5daa2
Mawinda <mawindavic@gmail.com>
2022-10-11 12:55:11 +0300

    adds room type converters fo list of ints and list of strings

--- data/src/main/java/com/mawinda/data/local/Converters.kt
+++ data/src/main/java/com/mawinda/data/local/Converters.kt
@@ -1,4 +1,30 @@
 package com.mawinda.data.local
 
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
 class Converters {
+
+    @TypeConverter
+    fun List<Int>?.listIntsToString(): String = if (this == null) "" else Gson().toJson(this)
+
+    @TypeConverter
+    fun String.toListOfInts(): List<Int> = if (this.isEmpty()) listOf() else Gson().fromJson(
+        this,
+        object : TypeToken<List<Int>>() {}.type
+    ) as List<Int>
+
+
+    @TypeConverter
+    fun List<String>?.listOfStringsToString(): String =
+        if (this == null) "" else Gson().toJson(this)
+
+    @TypeConverter
+    fun String.toListOfStrings(): List<String> = if (this.isEmpty()) listOf() else Gson().fromJson(
+        this,
+        object : TypeToken<List<String>>() {}.type
+    ) as List<String>
 }
+
+
